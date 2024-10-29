95a7015a113f10327d966133ab06826ea4f72867
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-10-29 11:21:54 -0600

    Creación de archivos de rutas
    Se crearon los archivos individuales para generar rutas.

--- HYPERFOCUS/bootstrap/app.php
+++ HYPERFOCUS/bootstrap/app.php
@@ -6,10 +6,31 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        using:function(){
+
+            Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/robert.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/isa.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/jose.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/sam.php'));
+
+            Route::middleware('web')
+            ->group(base_path('routes/ivan.php'));
+        },
     )
+
+
+    
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
---
+++ HYPERFOCUS/routes/robert.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/prueba', function () {
+//     return view('welcome');
+// });
+
+Route::view('/prueba','welcome');
