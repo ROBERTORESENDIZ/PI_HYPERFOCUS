dad531bf3b3184e7a6e1fd5a09214c0e6f0903c4
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-11-01 15:24:42 -0600

    Vista Principal Finalizada

--- HYPERFOCUS/resources/views/layouts/plantillaPrincipal.blade.php
+++ HYPERFOCUS/resources/views/layouts/plantillaPrincipal.blade.php
@@ -7,6 +7,8 @@
     @vite(['resources/js/app.js'])
     <link href="{{ asset('/css/plantillaPrincipal.css') }}" rel="stylesheet">
 
+    <!-- fuente-->
+    <link href="https://fonts.googleapis.com/css2?family=Lily+Script+One&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,400;1,700&family=Ruluko&display=swap" rel="stylesheet">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
