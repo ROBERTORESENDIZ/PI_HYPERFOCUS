667a2a5757c7fc4510fd9a5bfd5c55cf1c2302ec
Roberto Resendiz <robertoresendiz504@gmail.com>
2024-11-15 15:13:08 -0600

    Avance funcional Home

--- HYPERFOCUS/routes/web.php
+++ HYPERFOCUS/routes/web.php
@@ -14,7 +14,7 @@ Route::view('/preguntas','preguntasf')->name('rutapreguntas');
 Route::view('/iniciarsesion','iniciarSesion')->name('rutainiciarsesion');
 Route::view('/registrarse','registrarse')->name('rutaregistrarse');
 
-Route::view('/home','home')->name('rutahome');
+// Route::view('/home','home')->name('rutahome');
 Route::view('/misemana','miSemana')->name('rutamisemana');
 Route::view('/concentracion','concentracion')->name('rutaconcentracion');
 Route::view('/memoria','memoria')->name('rutamemoria');
