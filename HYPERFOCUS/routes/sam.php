59dd82ab0e5be1127a319a73041847797f7be475
SamuelVeytia <samveytia6@gmail.com>
2024-11-22 03:14:49 -0600

    CRUD DE MEMORIA
    Se agrego funcionalidad para inserts

--- HYPERFOCUS/routes/sam.php
+++ HYPERFOCUS/routes/sam.php
@@ -1,17 +1,14 @@
 <?php
 
+use App\Http\Controllers\memoriaController;
 use Illuminate\Support\Facades\Route;
+
+
 
 // Route::get('/prueba', function () {
 //     return view('welcome');
 // });
 
-Route::view('/prueba','welcome');
-
-
-
-
-
-
+//Route::view('/prueba','welcome');
 
 
--- HYPERFOCUS/routes/web.php
+++ HYPERFOCUS/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\memoriaController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -17,7 +18,13 @@ Route::view('/registrarse','registrarse')->name('rutaregistrarse');
 // Route::view('/home','home')->name('rutahome');
 Route::view('/misemana','miSemana')->name('rutamisemana');
 Route::view('/concentracion','concentracion')->name('rutaconcentracion');
-Route::view('/memoria','memoria')->name('rutamemoria');
+//Route::view('/memoria','memoria')->name('rutamemoria'); -> Es la que estoy trabajando en CRUD (samuel) la ruta  nueva esta abajo
 Route::view('/mislogros','misLogros')->name('rutamislogros');
+
+
+
+//Nuevas rutas para CRUD (samuel)
+Route::get('/memoria',[memoriaController::class,'index'])->name('rutamemoria');
+Route::post('/memoria/insert',[memoriaController::class,'store'])->name('rutainsertconjunto');
 
 
